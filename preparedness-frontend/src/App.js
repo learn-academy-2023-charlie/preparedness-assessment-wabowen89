6bb881fdb35e8ab8ab4c48bc6db8d080b766927a
wabowen89 <wabowen89@gmail.com>
2023-06-08 14:23:58 -0700

    updated App.js with Form and FormGroup

--- preparedness-frontend/src/App.js
+++ preparedness-frontend/src/App.js
@@ -1,33 +1,38 @@
 import React, {useState} from "react"
-import { Button, Input, Label } from "reactstrap"
+import { Button, Input, Label, Form, FormGroup } from "reactstrap"
 import ModalComponent from "./components/ModalComponent"
 import "./App.css"
 
 const App = () => {
 
+  // sets state for modal/username input
   const [modal, setModal] = useState(false)
   const [user, setUser] = useState("")
 
-  const handleUser = (event) => setUser(event.target.value)
+  // sets user state as inputs from the user
+  const handleChange = (e) => setUser(e.target.name)
   
+  // toggles the modal presentation on and off
   const toggle = () => setModal(!modal)
 
+  // clears the input field
   const clearField = () => setUser("")
 
   return (
     <div className="entire-content">
       <h1>Preparedness Assessment</h1>
-      <div className="form">
-        <div className="input">
+
+      <Form className="form">
+        <FormGroup className="input">
           <Label for="name">Enter your name</Label>
-          <Input id="input-field" value = {user} onChange = {handleUser}/>
-        </div>
+          <Input id="input-field" name = {user} onChange = {handleChange}/>
+        </FormGroup>
         <div className="button-container">
           <Button id="button" onClick = {toggle}>Click Me</Button>
           <Button id="button" onClick = {clearField}>Reset</Button>
           <ModalComponent isOpen = {modal} toggle = {toggle} user = {user}/>
         </div>
-      </div>
+      </Form>
     </div>
   )
 }
